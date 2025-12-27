ebb5173ab38684e2053d4dc44d1d1b7e5027b59a
abidurnurohi <abidurnurohi@gmail.com>
2025-12-28 02:49:22 +0700

    deployment

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port = process.env.PORT || 3000;
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./paw-mart-firebase-adminsdk-eceead1def.json");
+// index.js
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -193,7 +195,7 @@ async function run() {
 
 
 
-        await client.db("admin").command({ ping: 1 });
+        //await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
     finally {
